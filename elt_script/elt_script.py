b413fce4874e380345f64cdba51c1fc11f9085ab
Vincii22 <arvingm1522@gmail.com>
2025-08-19 16:37:11 +0800

    Fixed loop condition

--- elt_script/elt_script.py
+++ elt_script/elt_script.py
@@ -1,10 +1,10 @@
 import subprocess 
 import time 
 
-def wait_for_postgres(host, max_retries=5, delay_seconds=5):
+def wait_for_postgres(host, max_retries=10, delay_seconds=5):
     # Wait for PostgreSQL to become available. 
     retries = 0
-    while retries > max_retries:
+    while retries < max_retries:
         try:
             result = subprocess.run(
                 ["pg_isready", "-h", host], check = True, capture_output = True, text = True)
