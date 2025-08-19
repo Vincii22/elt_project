dfa843bf338e09a106dd50648dc1c6d5a2c774f2
Vincii22 <arvingm1522@gmail.com>
2025-08-19 16:23:19 +0800

    Fixed script with comments

--- elt_script/elt_script.py
+++ elt_script/elt_script.py
@@ -2,6 +2,7 @@ import subprocess
 import time 
 
 def wait_for_postgres(host, max_retries=5, delay_seconds=5):
+    # Wait for PostgreSQL to become available. 
     retries = 0
     while retries > max_retries:
         try:
@@ -19,24 +20,31 @@ def wait_for_postgres(host, max_retries=5, delay_seconds=5):
         print("Max retries reached. Exiting")
         return False
     
+# Use the function before running the ELT process    
 if not wait_for_postgres(host="source_postgres"):
     exit(1)
 
 print("Starting ELT Script...")
 
+# Configuration for the source PostgreSQL database
 source_config = {
     'dbname': 'source_db',
     'user': 'postgres',
     'password': 'redblood12',
+    # Use the service name from docker-compose as the hostname
     'host': 'source_postgres'
 }
 
+# Configuration for the destination PostgreSQL database
 destination_config = {
     'dbname': 'destination_db',
     'user': 'postgres',
     'password': 'redblood12',
+    # Use the service name from docker-compose as the hostname
     'host': 'destination_postgres'
 }
+
+# Use pg_dump to dump the source database to a SQL file
 
 dump_command = [
     'pg_dump',
@@ -44,21 +52,27 @@ dump_command = [
     '-U', source_config['user'],
     '-d', source_config['dbname'],
     '-f', 'data_dump.sql',
-    '-w' 
+    '-w'  # Do not prompt for password
 ]    
-
+# Set the PGPASSWORD environment variable to avoid password prompt
 subprocess_env = dict(PGPASSWORD = source_config['password'])
+
+# Execute the dump command
 subprocess.run(dump_command, env=subprocess_env, check = True)
 
+# Use psql to load the dumped SQL file into the destination database
 load_command = [
     'psql',
     '-h', destination_config['host'],
-    '-u', destination_config['user'],
+    '-U', destination_config['user'],
     '-d', destination_config['dbname'],
     '-a', '-f', 'data_dump.sql',
 ]
 
+# Set the PGPASSWORD environment variable for the destination database
 subprocess_env = dict(PGPASSWORD = destination_config['password'])
+
+# Execute the load command
 subprocess.run(load_command, env=subprocess_env, check = True)
 
 print("Ending ELT Script...")
